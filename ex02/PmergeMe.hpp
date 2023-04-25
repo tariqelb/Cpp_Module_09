e2f460f23b912ed06cb286cb66f91d0fee36cac7
Tariq El bouhali <tariqelbouhali039@gmail.com>
2023-04-25 10:38:22 +0100

    v2.3

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tel-bouh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 10:17:04 by tel-bouh          #+#    #+#             */
+/*   Updated: 2023/04/25 10:34:34 by tel-bouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+class PmergeMe
+{
+	private:
+		std::vector<int>	vec_nbr;
+		std::deque<int>		deq_nbr;
+
+	public:
+		PmergeMe();
+		PmergeMe(PmergeMe& rhs);
+		PmergeMe&	operator=(PmergeMe& rhs);
+		~PmergeMe();
+
+		std::vector<int>&		GetVectorNumbers(void);
+		std::deque<int>&		GetDequeNumbers(void);
+		void					FillContainers(int ac, char **av);
+		int						CheckForErrors(int ac, char **av);
+		void					DisplaySortedResult(void);
+
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tel-bouh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/25 10:30:31 by tel-bouh          #+#    #+#             */
+/*   Updated: 2023/04/25 10:36:02 by tel-bouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+int	main(int ac, char **av)
+{
+	Pmerge	number;
+
+	if (ac < 2)
+	{
+		std::cerr << "Error" << std::endl;
+		return (1);
+	}
+	if (number.CheckForErrors(ac, av))
+	{
+		std::cerr << "Error" << std::endl;
+		return (1);
+	}
+	number.FillContainers(ac, av);
+	number.DisplaySortedResult();
+	return (0);
+}
